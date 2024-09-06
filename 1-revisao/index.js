3302ee540c682e881a72bbce5e29839be8236ec8
Vinícius Maeda <viniciusmaeda@gmail.com>
2024-09-06 14:21:04 -0400

    revisão

--- 1-revisao/index.js
+++ 1-revisao/index.js
@@ -12,8 +12,8 @@ function subtrair(n1, n2) {
   return r;
 }
 
-let numero1 = 3;
-let numero2 = 5;
+let numero1 = 15;
+let numero2 = 9;
 
 console.log('A soma de 3 + 5 é: ', somar(numero1, numero2));
 console.log('A subtração de 5 - 3 é: ', subtrair(numero2, numero1));
@@ -38,3 +38,11 @@ const dividir = (n1, n2) => n1 / n2;
 
 console.log('A multiplicação de 3 * 5 é: ', multiplicar(numero1, numero2));
 console.log('A divisão de 5 / 3 é: ', dividir(numero2, numero1));
+
+
+// ---------------------------------------------------
+// Template String
+// console.log('A soma de 3 + 5 é: ', somar(numero1, numero2));
+// console.log('A soma de ', numero1, ' + ', numero2, ' é: ', somar(numero1, numero2));
+console.log(`A soma de ${numero1} + ${numero2} é: ${somar(numero1, numero2)}`);
+
