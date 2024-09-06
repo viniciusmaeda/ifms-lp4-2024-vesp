9668356781ecf73349bbbf0ce0060cfe7308b85b
Vinícius Maeda <viniciusmaeda@gmail.com>
2024-09-06 15:07:59 -0400

    projeto simples de servidor http

---
+++ 2-nodejs-http-nodemon/server.js
@@ -0,0 +1,20 @@
+// importar o pacote http
+const { renameSync } = require('fs');
+const http = require('http');
+
+// definição dos meios de acesso ao servidor
+let localhost = '127.0.0.1';
+let port = 3000;
+
+// configuração do servidor
+const server = http.createServer((requisicao, resposta) => {
+  resposta.statusCode = 200;
+  resposta.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  resposta.write('Olá, este é o servidor HTTP e está rodando localmente.\nTeste de pular linha.\nOutra linha do meu servidor.');
+  resposta.end();
+});
+
+// criar uma "escuta" no endereço e porta
+server.listen(port, localhost, console.log('O servidor está rodando...'));
+
+
