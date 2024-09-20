0d1fb7294d820e1a63ea5cefaf149f35aec7741c
Vinícius Maeda <viniciusmaeda@gmail.com>
2024-09-20 15:55:38 -0400

    servidor para somar dois números. atualização do readme

--- 2-nodejs-http-nodemon/server-calc.js
+++ 2-nodejs-http-nodemon/server-calc.js
@@ -1,3 +1,8 @@
+/*
+* Criar um servidor que receba 2 números pela url e informe a soma
+* http://127.0.0.1:3000/?n1=5&n2=3
+*/
+
 // importação dos pacotes/bibliotecas
 const http = require('http');
 const url = require('url'); // manipular o endereço url
@@ -9,24 +14,19 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   // 2 instruções para o navegador
   res.statusCode = 200; // código de status Ok
-  res.setHeader('Content-Type', 'text/plan; charset=utf-8');
+  res.setHeader('Content-Type', 'text/html; charset=utf-8'); // formato da resposta em html
 
   const q = url.parse(req.url, true).query; // manipular a url extraindo os parâmetros
 
-  // Parâmetros enviados através da url
-  // http://127.0.0.1:3000/?nome=vinicius&sobrenome=maeda&email=vinicius@email.com
-
-  // let parametros = 'Parâmetro 1: ' + q.p1;
-  let parametros = `
-    Nome: ${q.nome}
-    Sobrenome: ${q.sobrenome}
-    Email: ${q.email}
+  // extrair os parâmetros da url e converter para números
+  let n1 = Number(q.n1);
+  let n2 = Number(q.n2);
+  let resposta = `
+    A <u>soma</u> de ${n1} + ${n2} é igual a <b>${n1 + n2}</b>.
   `;
-  
 
   // mensagem que irá aparecer no corpo da página (<body>)
-  res.write(parametros);
-  // res.write('Hello world!!! You reach the server.');
+  res.write(resposta);
   res.end(); // finalizar a resposta
 });
 
